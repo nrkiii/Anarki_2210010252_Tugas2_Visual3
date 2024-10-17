ad30e6e259d9aa9c4529519a067e6c4b0fc0068d
nrkiii <120319196+nrkiii@users.noreply.github.com>
2024-10-17 12:07:10 +0800

    Add files via upload

---
+++ Qt_PyQt5/cara2.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+app = QApplication([])
+window = QPushButton("MyButton")
+window.show()
+app.exec_()
---
+++ Qt_PyQt5/cara3.py
@@ -0,0 +1,6 @@
+from PyQt5 import QtWidgets as qtw
+
+app = qtw.QApplication([])
+window = qtw.QPushButton("MyButton")
+window.show()
+app.exec_()
